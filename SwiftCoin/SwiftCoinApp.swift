cdbbbaab3e161461af02deacc5ecfc38748f80c2
abubakarMana01 <ym.abubakr7@gmail.com>
2023-03-15 23:15:22 +0100

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Abubakar Mana on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
